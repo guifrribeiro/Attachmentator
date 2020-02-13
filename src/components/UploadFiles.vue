496681b9faf7a2f236a4f68c497dff5952e1bc56
gferreira <gferreira@landix.com.br>
2020-02-13 10:19:25 -0300

    Upload file implementation

---
+++ src/components/UploadFiles.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="large-12 medium-12 small-12 cell">
+            <label>File
+                <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+            </label>
+            <button v-on:click="submitFile()">Submit</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            file: ''
+        }
+    },
+    methods: {
+        handleFileUpload() {
+            this.file = this.$refs.file.files[0];
+
+        },
+        submitFile() {
+            let formData = new FormData();
+        }
+    }
+}
+</script>
